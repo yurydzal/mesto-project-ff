f68d659915f79a17a74ecb5edf0d6a3fd496b75b
Yury Dzal <yurydzal@yandex.ru>
2023-12-06 05:51:59 +0400

    test

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,26 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+const cardTemplate = document.querySelector('#card-template');
+const places = document.querySelector('.places__list');
+
+function cardAddition(cardName, cardLink) {
+
+    const cardElement = cardTemplate.content.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = cardLink;
+    cardElement.querySelector('.card__image').alt = cardName;
+    cardElement.querySelector('.card__title').textContent = cardName;
+
+    cardElement.querySelector('.card__delete-button').addEventListener('click', function (evt) {
+        evt.target.parentElement.remove();
+    });
+
+    return places.append(cardElement);
+}
+
+initialCards.forEach(element => {
+    cardAddition(element.name, element.link);
+});
